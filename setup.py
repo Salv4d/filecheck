ce87cd47c9a782d0af6d026cfe1d8ffdc273ca12
Salvador Oliveira <slsalvadorlucas@gmail.com>
2022-01-20 21:46:20 -0400

    fix package name in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
+#!/usr/bin/python3
 from setuptools import setup, find_packages
 
 setup(
     name="filecheck",
-    version="0.0.2",
+    version="0.0.3",
     description="Verify if a new version of a file is available and update the file if true.",
     author="Salvador Oliveira",
     author_email="slsalvadorlucas@gmail.com",
     py_modules=[""],
     package_dir={"": "src"},
     packages=find_packages(
-        include=["datacheck"],
+        include=["filecheck"],
     ),
     classifiers=[
         "Programming Language :: Python :: 3",
